80ff19bcebaf35806b421a5569c1551f056e7036
Mario Bhona Parte <113872631+mariobnp@users.noreply.github.com>
2025-12-22 21:56:31 +0700

    feat(db) create table dan ngisi tabel di database (#1)

---
+++ database/migrations/2025_12_22_123207_create_konsultasi_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('konsultasi', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('mahasiswa_id')->constrained('mahasiswa')->onDelete('cascade');
+            $table->timestamp('tanggal');
+            $table->string('diagnosis')->nullable();
+            $table->float('cf_total')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('konsultasi');
+    }
+};
