a26681ff5bc8a679f17b572ce8be9b30cbe7c520
Mario Bhona Parte <113872631+mariobnp@users.noreply.github.com>
2025-12-22 22:34:08 +0700

    feat(db) make model and table relation (#2)
    * feat(db) create table dan ngisi tabel di database
    
    * feat(db) make model for table relation

--- app/Models/Rule.php
+++ app/Models/Rule.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Rule extends Model
 {
-    //
+    use HasFactory;
+    
+    protected $fillable = ['gejala_id', 'penyakit_id', 'cf_pakar'];
+
+    //relasi
+    public function penyakit()
+    {
+        return $this->belongsTo(Penyakit::class);
+    }
+
+    public function gejala()
+    {
+        return $this->belongsTo(Gejala::class);
+    }
 }
