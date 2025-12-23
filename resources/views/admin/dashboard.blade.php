fd2fc2856d00f685bf880126b6828b4e1b7f8d41
Mario Bhona Parte <113872631+mariobnp@users.noreply.github.com>
2025-12-23 20:36:57 +0700

    Wisnu/frontend buat todo untuk frontend (#4)
    * buat todo
    
    * buat todo
    
    ---------
    
    Co-authored-by: Wisnu <wisnu3kac@gmail.com>

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,7 +3,13 @@
 
 @section('content')
 <h2>Dashboard Admin</h2>
+<!-- BUAT NISA: -->
+<!-- TODO: Buat table isi kolomnya = No,Nama,Usia,Jenis Kelamin,Alamat,email,no telepon,Aksi-->
+<!-- TODO: Dibagian aksi buat dua Tombol Edit Dan Hapus -->
 
+<!-- ------------------------- -->
+ 
+<!-- TODO buat mario -->
 <!-- TODO: Tampilkan ringkasan jumlah mahasiswa, gejala, penyakit -->
 <!-- TODO: Tambahkan link cepat ke CRUD gejala, penyakit, mahasiswa -->
 <!-- TODO: Gunakan card / chart jika UI/UX menyarankan -->
--- resources/views/admin/penyakit.blade.php
+++ resources/views/admin/penyakit.blade.php
@@ -1,9 +1,17 @@
-@extends('layouts.app')
+
+?@extends('layouts.app')
 @section('title', 'CRUD Penyakit')
 
 @section('content')
 <h2>Daftar Penyakit</h2>
 
+<!-- BUAT NISA: -->
+<!-- TODO: Buat table isi kolomnya = No,Kode_penyakit,Nama_penyakit-->
+<!-- TODO: Dibagian aksi buat dua Tombol Edit Dan Hapus -->
+
+<!-- ------------------------- -->
+
+<!-- TODO buat mario -->
 <!-- TODO: Tabel daftar penyakit -->
 <!-- TODO: Tombol Tambah / Edit / Hapus -->
 <!-- TODO: Modal atau halaman untuk form tambah/edit -->
